69ae98c6e6f72a288b0521d179c1a6256de3adfc
Bijinaga24 <babastian1024@gmail.com>
2025-05-09 14:15:21 +0700

    menambahakan file access

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const http = require('http');
-const server = http.createServer((req, res) =>{
-    res.end('welcome to node.js')
+const fileSys = require('fs');
+    
+const server = http.createServer((req, res) => {
+    fileSys.readFile('pages/index.html', (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+            return;
+        }
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+    });
 });
+
 server.listen(8000);
