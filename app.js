7fdbfc84224650b933ad59bee325164a68e7be1f
Bijinaga24 <babastian1024@gmail.com>
2025-05-09 16:02:10 +0700

    File URL & Navigation

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-const http = require('http');
-const fileSys = require('fs');
-    
-const server = http.createServer((req, res) => {
-    fileSys.readFile('pages/index.html', (err, data) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-            return;
-        }
+const http = require("http");
+const fileSys = require("fs");
+const url = require("url");
 
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        res.end();
-    });
+const server = http.createServer((req, res) => {
+  let q = url.parse(req.url, true);
+  let menu = q.query.menu;
+  let fileLocation;
+
+  switch (menu) {
+    case "home":
+      fileLocation = "pages/index.html";
+      break;
+    case "about":
+      fileLocation = "pages/about.html";
+      break;
+    default:
+      fileLocation = "pages/index.html";
+  }
+
+  fileSys.readFile(fileLocation, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Page Not Found");
+      return;
+    }
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    res.end();
+  });
 });
 
-server.listen(8000);
+server.listen(8000, () => {
+  console.log("Server running on http://localhost:8000");
+});
